a301b2f9d354bbb73f6d3b594b4e56206fc7c418
Liza <lkorn036@gmail.com>
2024-09-02 18:03:12 -0500

    indents are aligned

--- src/js/__tests__/object.test.js
+++ src/js/__tests__/object.test.js
@@ -10,55 +10,55 @@ test("should return properties in specified order and then alphabetically", () =
   };
   const sortedObj = orderByProps(obj, ["name", "level"]);
   const result = [
-    { key: "name", value: "мечник" }, 
-    { key: "level", value: 2 }, 
-    { key: "attack", value: 80 }, 
-    { key: "defence", value: 40 }, 
-    { key: "health", value: 10 }, 
+    { key: "name", value: "мечник" },
+    { key: "level", value: 2 },
+    { key: "attack", value: 80 },
+    { key: "defence", value: 40 },
+    { key: "health", value: 10 },
   ];
   expect(sortedObj).toEqual(result);
 });
 
 test("handles case with no sorting order", () => {
-    const obj = {
-      name: "мечник",
-      level: 2,
-      health: 10,
-      attack: 80,
-      defence: 40,
-    };
-    const sortedObj = orderByProps(obj, ["name", "level"]);
-    const result = [
-      { key: "name", value: "мечник" }, 
-      { key: "level", value: 2 }, 
-      { key: "attack", value: 80 }, 
-      { key: "defence", value: 40 }, 
-      { key: "health", value: 10 }, 
-    ];
-    expect(sortedObj).toEqual(result);
-  });
+  const obj = {
+    name: "мечник",
+    level: 2,
+    health: 10,
+    attack: 80,
+    defence: 40,
+  };
+  const sortedObj = orderByProps(obj, ["name", "level"]);
+  const result = [
+    { key: "name", value: "мечник" },
+    { key: "level", value: 2 },
+    { key: "attack", value: 80 },
+    { key: "defence", value: 40 },
+    { key: "health", value: 10 },
+  ];
+  expect(sortedObj).toEqual(result);
+});
 
-  test("handles case with no matching keys in the order array", () => {
-    const obj = {
-      name: "мечник",
-      level: 2,
-      health: 10,
-      attack: 80,
-      defence: 40,
-    };
-    const sortedObj = orderByProps(obj, ["speed", "weight"]);
-    const result = [
-      { key: "attack", value: 80 }, 
-      { key: "defence", value: 40 }, 
-      { key: "health", value: 10 },
-      { key: "level", value: 2 }, 
-      { key: "name", value: "мечник" },
-    ];
-    expect(sortedObj).toEqual(result);
-  });
+test("handles case with no matching keys in the order array", () => {
+  const obj = {
+    name: "мечник",
+    level: 2,
+    health: 10,
+    attack: 80,
+    defence: 40,
+  };
+  const sortedObj = orderByProps(obj, ["speed", "weight"]);
+  const result = [
+    { key: "attack", value: 80 },
+    { key: "defence", value: 40 },
+    { key: "health", value: 10 },
+    { key: "level", value: 2 },
+    { key: "name", value: "мечник" },
+  ];
+  expect(sortedObj).toEqual(result);
+});
 
-  test("returns empty array for an empty object", () => {
-    const obj = {};
-    const sortedObj = orderByProps(obj, ["speed", "weight"]);
-    expect(sortedObj).toEqual([]);
-  });
+test("returns empty array for an empty object", () => {
+  const obj = {};
+  const sortedObj = orderByProps(obj, ["speed", "weight"]);
+  expect(sortedObj).toEqual([]);
+});
--- src/js/object.js
+++ src/js/object.js
@@ -1,24 +1,23 @@
 export default function orderBykeys(obj, order) {
   const result = [];
-  order.forEach(key => {
+  order.forEach((key) => {
     if (Object.prototype.hasOwnProperty.call(obj, key)) {
       result.push({ key, value: obj[key] });
     }
   });
   const anotherKeys = [];
   for (const key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key) && !order.includes(key)) {
-        anotherKeys.push(key);
+    if (
+      Object.prototype.hasOwnProperty.call(obj, key) &&
+      !order.includes(key)
+    ) {
+      anotherKeys.push(key);
     }
   }
   anotherKeys.sort();
 
-  anotherKeys.forEach(key => {
-    result.push({key, value: obj[key]});
-  })
+  anotherKeys.forEach((key) => {
+    result.push({ key, value: obj[key] });
+  });
   return result;
 }
-
-
-
-
